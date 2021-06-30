e0ed9ff93dddef902db6c978594218437f50f967
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-06-29 21:52:23 -0400

    works providing list of census tracts

--- src/index.js
+++ src/index.js
@@ -1,20 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { json } from 'd3';
+/*
+const getData = ({ url }) => {
 
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    json(url)
+    .then((d) => {
+      setData(d);
+    })
+  }, [])
+
+  return(data);
+}
+*/
 const App = () => {
+
+  const [data, setData] = useState([]);
 
   const jsonURL = 'https://raw.githubusercontent.com/rahulAgrBej/d3_map_example/master/data/mi_tract.json'
 
-  useEffect(()=> {
-    json(jsonURL)
-      .then((data) => {
-        console.log(data);
-      })
-  }, [])
+  useEffect(() => {
+    json(jsonURL).then((d) =>{
+      console.log(d);
+      setData(d.features);
+    })
+  }, []);
 
+  if (!data) {
+    return(<h1>not loaded yet</h1>)
+  }
+
+  console.log('here bfore return')
+
+  
   return(
+    <React.Fragment>
     <h1>Testing</h1>
+    <ul>
+        {
+          data.map((d) => {
+            return <li key={d.properties.GEOID}>{d.properties.NAME}</li>
+          })
+        }
+    </ul>
+    </React.Fragment>
+    
   )
 }
 
