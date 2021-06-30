fdceb116956848bae2115f72f31b43813ad7c511
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-06-29 20:37:03 -0400

    new census tracts for michigan with all properties

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { json } from 'd3';
+
+const App = () => {
+
+  const jsonURL = 'https://raw.githubusercontent.com/rahulAgrBej/d3_map_example/master/data/mi_tract.json'
+
+  useEffect(()=> {
+    json(jsonURL)
+      .then((data) => {
+        console.log(data);
+      })
+  }, [])
+
+  return(
+    <h1>Testing</h1>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+/*
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +38,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
